772e336ed8018d3e5a7a4e09f2b036668554af13
Aria Ghora Prabono <hello@ghora.net>
2019-06-21 03:28:52 +0900

    implement SVD
    - interface with LAPACKE_dgesvd
    - add Diag function

--- src/dtlinalg.pas
+++ src/dtlinalg.pas
@@ -8,9 +8,31 @@ interface
 uses
   Classes, SysUtils, Math, DTCore;
 
+{ @abstract(Calculate SVD of X)
+
+  Store the result in U, Sigma, and VT respectively. }
+procedure SVD(X: TDTMatrix; var U: TDTMatrix; var Sigma: TDTMatrix; var VT: TDTMatrix);
 
 implementation
 
+procedure SVD(X: TDTMatrix; var U: TDTMatrix; var Sigma: TDTMatrix; var VT: TDTMatrix);
+var
+  Superb: TFloatVector;
+begin
+  SetLength(Superb, Min(X.Width, X.Height) - 1);
+  SetLength(Sigma.val, X.Width);
+  SetLength(U.val, X.Height * X.Height);
+  SetLength(VT.val, X.Width * X.Width);
+
+  Sigma.Width := X.Width;
+  Sigma.Height := 1;
+  U.Width := X.Height;
+  U.Height := X.Height;
+  VT.Width := X.Width;
+  VT.Height := X.Width;
+
+  LAPACKE_dgesvd(CblasRowMajor, 'A', 'A', X.Height, X.Width, CopyMatrix(X).val,
+    X.Width, Sigma.val, U.val, X.Height, VT.val, X.Width, Superb);
+end;
 
 end.
-
