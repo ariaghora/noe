9337ec511f0a32972ae9e12989e749122b3f78be
Aria Ghora Prabono <hello@ghora.net>
2020-01-28 00:30:03 +0700

    implement sgd optimizer

---
+++ src/noe.optimizer.pas
@@ -0,0 +1,54 @@
+unit noe.optimizer;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, noe.core, noe.autograd;
+
+type
+
+  { TBaseOptimizer }
+
+  TBaseOptimizer = class
+  private
+    FLearningRate: double;
+    FLoss:   TVariable;
+  public
+    constructor Create(ALearningRate: double);
+    procedure UpdateParams(params: array of TVariable); overload; virtual; abstract;
+    property LearningRate: double read FLearningRate write FLearningRate;
+    property Loss: TVariable read FLoss write FLoss;
+  end;
+
+  { TGradientDescentOptimizer }
+
+  TGradientDescentOptimizer = class(TBaseOptimizer)
+    procedure UpdateParams(ModelParams: array of TVariable); override;
+  end;
+
+implementation
+
+{ TBaseOptimizer }
+
+constructor TBaseOptimizer.Create(ALearningRate: double);
+begin
+  self.LearningRate := ALearningRate;
+end;
+
+{ TGradientDescentOptimizer }
+
+procedure TGradientDescentOptimizer.UpdateParams(ModelParams: array of TVariable);
+
+var
+  param: TVariable;
+begin
+  for param in ModelParams do
+  begin
+    param.Data := param.Data - self.LearningRate * param.Grad;
+    param.ZeroGrad;
+  end;
+end;
+
+end.
