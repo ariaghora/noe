df49f771489ae2e74b09d9fc5ffe4a1abc525c44
Aria Ghora Prabono <hello@ghora.net>
2020-08-02 17:35:12 +0900

    Add tests for Max op

--- tests/testgrad.pas
+++ tests/testgrad.pas
@@ -11,6 +11,8 @@ type
 
   TTestGrad = class(TTestCase)
   published
+    procedure TestGradMax1;
+    procedure TestGradMax2;
     procedure TestGradMean;
     procedure TestGradMeanAxis;
     procedure TestGradSigmoid;
@@ -22,6 +24,29 @@ var
   T, U: TTensor;
 
 implementation
+
+procedure TTestGrad.TestGradMax1;
+begin
+  T := TMultiArray([1, 2, 3, 4, 5, 6]).Reshape([2, 3]);
+  T.RequiresGrad := True;
+  U := Max(T);
+  U.Backward(Ones(U.Shape));
+  AssertTrue(ArrayEqual(T.Grad,
+                        TMultiArray([0, 0, 0,
+                                     0, 0, 1]).Reshape([2, 3])
+                        ));
+end;
+
+procedure TTestGrad.TestGradMax2;
+begin
+  T := TMultiArray([6, 6, 6, 6, 6, 6]).Reshape([2, 3]);
+  T.RequiresGrad := True;
+  U := Max(T);
+  U.Backward(Ones(U.Shape));
+  AssertTrue(ArrayEqual(T.Grad,
+                        Ones([2, 3])
+                        ));
+end;
 
 procedure TTestGrad.TestGradMean;
 begin
